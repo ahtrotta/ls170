243e06897658f9a563207c655cab631b58b1913f
ahtrotta <ahtrotta@gmail.com>
2021-01-27 14:31:29 -0500

    added headers to responses

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -18,18 +18,25 @@ function server () {
     path=${message_arr[1]}
     if [[ $method = 'GET' ]]
     then
-      if [[ -f "./www/$path" ]]
+      if [[ -f "./www$path" ]]
       then
-        echo -ne 'HTTP/1.1 200 OK\r\n\r\n'; cat "./www/$path"
+        body_size=$(wc -c < ./www$path)
+        echo -ne 'HTTP/1.1 200 OK\r\n' 
+        echo -ne 'Content-Type: text/html; charset=utf-8\r\n'
+        echo -ne 'Content-Length: '$body_size
+        echo -ne '\r\n\r\n'
+        cat "./www$path"
       else
-        echo -ne 'HTTP/1.1 404 Not Found\r\n\r\n'
+        echo -ne 'HTTP/1.1 404 Not Found\r\n'
+        echo -ne 'Content-Length: 0\r\n'
       fi
     else
-      echo -ne 'HTTP/1.1 400 Bad Request\r\n\r\n'
+      echo -ne 'HTTP/1.1 400 Bad Request\r\n'
+      echo -ne 'Content-Length: 0\r\n'
     fi
   done
 }
 
 coproc SERVER_PROCESS { server; }
 
-netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+netcat -klv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
